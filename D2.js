7e69a391ab1f2658075a283de5a065d92604c6d4
Javier Ortiz <javier.ortiz989@gmail.com>
2020-04-07 14:00:12 -0700

    Complete 03 A

--- D2.js
+++ D2.js
@@ -21,11 +21,24 @@ function validIP(str) {
 // console.log(validIP("0.0.0.0.0"));
 // console.log(validIP("0.0.256.0"));
 
-
-// Write a method that reads in a file of integers, one per line, and sums them. Skip the line if it begins with a "#".
-function sumFromFile(file) {
-    let reader = new FileReader();
-    reader.readAsText(file);
+// Implement the Array.prototype.map function in JavaScript.
+Array.prototype.myMap = function(fn) {
+    const result = [];
+    this.forEach(el => {
+        result.push(fn(el));
+    })
+    return result;
 }
 
-sumFromFile("./myFile.md");
+console.log([1,2,3].myMap(el => el * 2));
+
+
+// In a SQL db, you have two tables, an employees table and a departments table. 
+// Employees belong to only one department. Write a SQL query that, given a department name, 
+// finds all the employees in that department.
+
+// SELECT *
+// FROM employees
+// JOIN departments
+// ON departments.id = emloyees.department_id
+// WHERE department.name = tech
